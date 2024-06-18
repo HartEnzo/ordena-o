d745d580e7d6c872a7d128ff05f5b3d95f38aa43
Enzo Guilherme <enzoguihartmann@gmail.com>
2024-06-18 08:51:25 -0300

    refactor: adiciona logica de remocao e ordenacao no primeiro for

--- index.js
+++ index.js
@@ -7,20 +7,13 @@ if(numeros[i] > numeros[j]){
     let aux = numeros[i]
     numeros[i]= numeros[j]
     numeros[j]= aux 
+}else if(numeros[i] == numeros[i]){
+    numeros.splice(j, 1)
+    j--
 }
 
 }
 }
 console.log(numeros)
 
-for (let i = 0; i < numeros.length; i++) {
-   
-    for (let j = i + 1; j < numeros.length; j++) {
-    if (numeros[i] == numeros[j]) {
-        numeros.splice(j, 1)
-    j--
-    }
-    }
-
-}
 
